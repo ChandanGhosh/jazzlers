1ee7f3cd5f8711913deb0cddf6083b83d052969f
chandanghosh <ckghosh1983@gmail.com>
2022-05-01 21:13:59 +0530

    factorial of number

--- src/com/chandanghosh/factorial/Factorial.java
+++ src/com/chandanghosh/factorial/Factorial.java
@@ -1,22 +1,32 @@
 package com.chandanghosh.factorial;
 
-public class Factorial
-{
-    public static void main(String[] args)
-    {
-        System.out.println(factorial(-4));
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the num: ");
+        int n = scanner.nextInt();
+        System.out.println("factorial in recursion: " + factorial_recursion(n));
+        System.out.println("factorial in loop: " + factorial_loop(n));
     }
 
-    private static int factorial(int n)
-    {
-        if (n < 0)
-        {
+    private static int factorial_recursion(int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("Number should not be negative");
         }
-        if (n == 0)
-        {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        return n * factorial(n - 1);
+        return n * factorial_recursion(n - 1);
+    }
+
+    private static int factorial_loop(int n) {
+        int i = 1;
+        while (n > 0) {
+            i *= n;
+            n--;
+        }
+        return i;
     }
 }
